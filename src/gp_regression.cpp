b29c5e2097ef620246c4de55d341d73a40637366
xcyan <xcyan@umich.edu>
2015-11-18 22:19:47 -0500

    2015/11/18

--- src/gp_regression.cpp
+++ src/gp_regression.cpp
@@ -5,6 +5,22 @@
  *      Author: zhangyuting
  */
 
+#include <string>
+#include "gp_regression.hpp"
+#include "base_detector.hpp"
 
+void fgs_gp_box_reg::Load( const std::string & model_path ) {
 
+  return;
+}
+
+void fgs_gp_box_reg::SetDetector(fgs_base_detector & detector_) {
+
+  return;
+}
+
+cv::Rect2d fgs_gp_box_reg::ProposeBox(std::vector< std::pair<cv::Rect2d, double>> known_boxes) {
+
+  return;
+}
 
