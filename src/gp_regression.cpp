a66a9d1a4bc5df4842637b4f652d25e83ec9f682
yutingzh <yutingzh@umich.edu>
2015-11-30 23:19:17 -0500

    add comments

--- src/gp_regression.cpp
+++ src/gp_regression.cpp
@@ -1,9 +1,32 @@
-/*
- * gp_regression.cpp
+/******************************************************************************
  *
- *  Created on: Nov 15, 2015
- *      Author: zhangyuting
- */
+ * This software is the core implementation of the fine-grained search (FGS) method
+ * based on the Gaussian process regression with latent variables.
+ *
+ * Details of the method can be found in the follow paper
+ *   Yuting Zhang, Kihyuk Sohn, Ruben Villegas, Gang Pan, Honglak Lee
+ *   ``Improving Object Detection with Deep Convolutional Networks via Bayesian Optimization and Structured Prediction''
+ *   IEEE Conference on Computer Vision and Pattern Recognition (CVPR), Boston, MA, 2015.
+ *
+ * The following people contributed to the code: 
+ *   Yuting Zhang   <zyt.cse@gmail.com>
+ *   Kihyuk Sohn    <kihyuks@umich.edu>
+ *   Ruben Villegas <rubville@umich.edu>
+ *   Xinchen Yan    <xcyan@umich.edu>
+ *   Gang Pan       <gpan@zju.edu.cn>
+ *   Honglak Lee    <honglak@eecs.umich.edu>
+ *
+ * This software is provided by the copyright holders and contributors "as is" and
+ * any express or implied warranties, including, but not limited to, the implied
+ * warranties of merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the Intel Corporation or contributors be liable for any direct,
+ * indirect, incidental, special, exemplary, or consequential damages
+ * (including, but not limited to, procurement of substitute goods or services;
+ * loss of use, data, or profits; or business interruption) however caused
+ * and on any theory of liability, whether in contract, strict liability,
+ * or tort (including negligence or otherwise) arising in any way out of
+ * the use of this software, even if advised of the possibility of such damage.
+*******************************************************************************/
 
 #include <string>
 #include <cmath>
@@ -356,11 +379,6 @@ void BoxReg::Load( const std::string & model_path ) {
 	}
 	return;
 }
-
-//void BoxReg::SetDetector(cv2::fgs_base_detector & detector_) {
-//
-//  return;
-//}
 
 cv::Rect BoxReg::ProposeBox(std::vector< std::pair<cv::Rect, double> > known_boxes) {
 	cv::Mat PsiN1; // 4 x N
