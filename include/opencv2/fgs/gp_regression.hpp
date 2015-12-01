b5e42a3521cf3bb1db093898e6ce19654fcc0b4b
Xinchen Yan <xcyan@umich.edu>
2015-11-30 19:55:05 -0500

    2015/11/30 checkpoint

--- include/opencv2/fgs/gp_regression.hpp
+++ include/opencv2/fgs/gp_regression.hpp
@@ -9,6 +9,9 @@
 #define INCLUDE_OPENCV2_FGS_GP_REGRESSION_HPP_
 
 #include <string>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
 #include "base_detector.hpp"
 #include "opencv2/opencv.hpp"
 
@@ -17,7 +20,7 @@ namespace cv2 {
 // argmax z on joint probability of the existing boxes
 // argmax yNp1 on conditional probability
 
-//	kNp1: 1 x N; KN: N x N; psiNp1: 4 x 1
+//	kNp1: N x 1; KN: N x N; psiNp1: 4 x 1
 
 class fgs_gp_box_reg {
 protected:
@@ -29,40 +32,43 @@ protected:
 		cv::Mat diagSqrtLambda; // 4 x 1
 		double normCov;
 		double noiseSigma2;
-		cv::Mat idxbScaleEnabled;
-	} GPmodel;
-	cv2::fgs_base_detector& detector;
+		cv::Mat idxbScaleEnabled; // 4 x 1
+	} GPmodel_;
+	cv2::fgs_base_detector& detector_;
 
 	/**
 	 * private/protected functions
 	 */
-	//	kNp1 = sgp_kNp1( psiNp1, PsiN, GPmodel );
 	cv::Mat sgp_kNp1_forward(const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
 	cv::Mat sgp_kNp1_backward(const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
-	//	mu   = sgp_posterior_mu( kNp1, KN, fN, GPmodel );
-	double sgp_posterior_mu_forward(const cv::Mat &, const cv::Mat &, const double &, const GPmodel_dfn &);
-	double sgp_posterior_mu_backward(const cv::Mat &, const cv::Mat &, const double &, const GPmodel_dfn &);
-	//	s2   = sgp_posterior_s2( kNp1, KN, GPmodel );
+	double sgp_posterior_mu_forward(const cv::Mat &, const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
+	cv::Mat sgp_posterior_mu_backward(const cv::Mat &, const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
 	double sgp_posterior_s2_forward(const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
-	double sgp_posterior_s2_backward(const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
-	//	a    = sgp_ei( mu, s2, fN_hat );
+	cv::Mat sgp_posterior_s2_backward(const cv::Mat &, const cv::Mat &, const GPmodel_dfn &);
 	double sgp_ei_forward(const double &, const double &, const double &);
-	double sgp_ei_backward(const double &, const double &, const double &);
-	// 	KN = sgp_KN( PsiN, GPmodel );
-	cv::Mat sgp_KN_forward(const cv::Mat &, const GPmodel_dfn &);
-	cv::Mat sgp_KN_backward(const cv::Mat &, const GPmodel_dfn &);
-	//	Cnoisy = sgp_cov(GPmodel, z, Psi1);
+	std::vector<double> sgp_ei_backward(const double &, const double &, const double &);
+	cv::Mat sgp_KN(const cv::Mat &, const GPmodel_dfn &);
 	cv::Mat sgp_cov_forward(const GPmodel_dfn &, const double &, const cv::Mat &);
 	cv::Mat sgp_cov_backward(const GPmodel_dfn &, const double &, const cv::Mat &);
-	//	a = sgp_neg_acquisition_ei(GPmodel, psiNp1, PsiN, fN, fN_hat, KN)
-	double sgp_neg_acquisition_ei_forward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &, const double &, const double &, const cv::Mat &);
-	double sgp_neg_acquisition_ei_backward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &, const double &, const double &, const cv::Mat &);
-	//	ll = sgp_neglogik(GPmodel, z, Psi1, f)
-	double sgp_neglogik_forward(const GPmodel_dfn &, const double &, const cv::Mat &, const cv::Mat &);
-	double sgp_neglogik_backward(const GPmodel_dfn &, const double &, const cv::Mat &, const cv::Mat &);
-	//	ll = sgp_neglogik_givenC(GPmodel, C, f)
-	double sgp_neglogik_givenC_forward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &);
-	double sgp_neglogik_givenC_backward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &);
+	double sgp_neg_acquisition_ei_forward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &, const cv::Mat &, const double &, const cv::Mat &);
+	cv::Mat sgp_neg_acquisition_ei_backward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &, const cv::Mat &, const double &, const cv::Mat &);
+	double sgp_negloglik_forward(const GPmodel_dfn &, const double &, const cv::Mat &, const cv::Mat &);
+	double sgp_negloglik_backward(const GPmodel_dfn &, const double &, const cv::Mat &, const cv::Mat &);
+	double sgp_negloglik_givenC_forward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &);
+	cv::Mat sgp_negloglik_givenC_backward(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &);
+
+	//utils;
+	//
+	double normpdf(const double &);
+	double normcdf(const double &);
+	double erf(const double &);
+	double erfc(const double &);
+	cv::Mat pdist(const cv::Mat &); // n x m --> n(n-1)/2 x 1
+	cv::Mat squareform(const cv::Mat &); //n(n-1)/2 x 1 --> n x n
+	cv::Mat rect2cvmat(cv::Rect &);
+	cv::Rect cvmat2rect(cv::Mat &);
+	double findMinZ(const GPmodel_dfn &, const double &, const cv::Mat &, const cv::Mat &);
+	cv::Mat findMinPsiNp1(const GPmodel_dfn &, const cv::Mat &, const cv::Mat &, const cv::Mat &, const double &, const cv::Mat &);
 
 public:
 	typedef cv2::fgs_gp_box_reg BoxReg;
